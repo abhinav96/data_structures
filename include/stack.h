825162554a12bf39403d413fe7243a43cdd827d0
Abhinav Valecha <valecha.abhinav@gmail.com>
2017-03-15 23:28:56 +0530

    Add push,pop,peek methods for primitives

--- include/stack.h
+++ include/stack.h
@@ -1,10 +1,58 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 typedef struct _stack* stack;
 
 stack new_stack();
-size_t stack_size(stack s);
+
 void stack_push(stack s, void *data, size_t size);
+void stack_push_char(stack s, char c);
+void stack_push_uchar(stack s, unsigned char c);
+void stack_push_short(stack s, short si);
+void stack_push_ushort(stack s, unsigned short si);
+void stack_push_int(stack s, int i);
+void stack_push_uint(stack s, unsigned int i);
+void stack_push_long(stack s, long l);
+void stack_push_ulong(stack s, unsigned long l);
+void stack_push_long_long(stack s, long long ll);
+void stack_push_ulong_long(stack s, unsigned long long ll);
+void stack_push_float(stack s, float f);
+void stack_push_double(stack s, double d);
+void stack_push_long_double(stack s, long double ld);
+void stack_push_string(stack s, char *str);
+
 void* stack_pop(stack s);
+char stack_pop_char(stack s);
+unsigned char stack_pop_uchar(stack s);
+short stack_pop_short(stack s);
+unsigned short stack_pop_ushort(stack s);
+int stack_pop_int(stack s);
+unsigned int stack_pop_uint(stack s);
+long stack_pop_long(stack s);
+unsigned long stack_pop_ulong(stack s);
+long long stack_pop_long_long(stack s);
+unsigned long long stack_pop_ulong_long(stack s);
+float stack_pop_float(stack s);
+double stack_pop_double(stack s);
+long double stack_pop_long_double(stack s);
+char* stack_pop_string(stack s);
+
 void* stack_peek(stack s);
+char stack_peek_char(stack s);
+unsigned char stack_peek_uchar(stack s);
+short stack_peek_short(stack s);
+unsigned short stack_peek_ushort(stack s);
+int stack_peek_int(stack s);
+unsigned int stack_peek_uint(stack s);
+long stack_peek_long(stack s);
+unsigned long stack_peek_ulong(stack s);
+long long stack_peek_long_long(stack s);
+unsigned long long stack_peek_ulong_long(stack s);
+float stack_peek_float(stack s);
+double stack_peek_double(stack s);
+long double stack_peek_long_double(stack s);
+char* stack_peek_string(stack s);
+
+bool stack_is_empty(stack s);
+size_t stack_size(stack s);
--- src/stack.c
+++ src/stack.c
@@ -35,10 +35,63 @@ void stack_push(stack s, void *data, size_t size) {
 	++s->size;
 }
 
+void stack_push_char(stack s, char c) {
+	stack_push(s, &c, sizeof(char));
+}
+
+void stack_push_uchar(stack s, unsigned char c) {
+	stack_push(s, &c, sizeof(unsigned char));
+}
+
+void stack_push_short(stack s, short si) {
+	stack_push(s, &si, sizeof(short));
+}
+
+void stack_push_ushort(stack s, unsigned short si) {
+	stack_push(s, &si, sizeof(unsigned short));
+}
+
+void stack_push_int(stack s, int i) {
+	stack_push(s, &i, sizeof(int));
+}
+
+void stack_push_uint(stack s, unsigned int i) {
+	stack_push(s, &i, sizeof(unsigned int));
+}
+
+void stack_push_long(stack s, long l) {
+	stack_push(s, &l, sizeof(long));
+}
+
+void stack_push_ulong(stack s, unsigned long l) {
+	stack_push(s, &l, sizeof(unsigned long));
+}
+
+void stack_push_long_long(stack s, long long ll) {
+	stack_push(s, &ll, sizeof(long long));
+}
+
+void stack_push_ulong_long(stack s, unsigned long long ll) {
+	stack_push(s, &ll, sizeof(unsigned long long));
+}
+
+void stack_push_float(stack s, float f) {
+	stack_push(s, &f, sizeof(float));
+}
+
+void stack_push_double(stack s, double d) {
+	stack_push(s, &d, sizeof(double));
+}
+
+void stack_push_long_double(stack s, long double ld) {
+	stack_push(s, &ld, sizeof(long double));
+}
+
+void stack_push_string(stack s, char *str) {
+	stack_push(s, str, strlen(str));
+}
+
 void* stack_pop(stack s) {
-	if (stack_size(s) == 0) {
-		return NULL;
-	}
 	stack_node fr = s->head;
 	void *data = fr->data;
 	s->head = s->head->next;
@@ -47,13 +100,165 @@ void* stack_pop(stack s) {
 	return data;
 }
 
-size_t stack_size(stack s) {
-	return s->size;
+char stack_pop_char(stack s) {
+	char *c = stack_pop(s);
+	char return_char = *c;
+	free(c);
+	return return_char;
+}
+
+unsigned char stack_pop_uchar(stack s) {
+	unsigned char *c = stack_pop(s);
+	unsigned char return_char = *c;
+	free(c);
+	return return_char;
+}
+
+short stack_pop_short(stack s) {
+	short *sh = stack_pop(s);
+	short return_short = *sh;
+	free(sh);
+	return return_short;
+}
+
+unsigned short stack_pop_ushort(stack s) {
+	unsigned short *sh = stack_pop(s);
+	unsigned short return_short = *sh;
+	free(sh);
+	return return_short;
+}
+
+int stack_pop_int(stack s) {
+	int *i = stack_pop(s);
+	int return_int = *i;
+	free(i);
+	return return_int;
+}
+
+unsigned int stack_pop_uint(stack s) {
+	unsigned int *i = stack_pop(s);
+	unsigned int return_int = *i;
+	free(i);
+	return return_int;
+}
+
+long stack_pop_long(stack s) {
+	long *l = stack_pop(s);
+	long return_long = *l;
+	free(l);
+	return return_long;
+}
+
+unsigned long stack_pop_ulong(stack s) {
+	unsigned long *l = stack_pop(s);
+	unsigned long return_long = *l;
+	free(l);
+	return return_long;
+}
+
+long long stack_pop_long_long(stack s) {
+	long long *ll = stack_pop(s);
+	long long return_long_long = *ll;
+	free(ll);
+	return return_long_long;
+}
+
+unsigned long long stack_pop_ulong_long(stack s) {
+	unsigned long long *ll = stack_pop(s);
+	unsigned long long return_long_long = *ll;
+	free(ll);
+	return return_long_long;
+}
+
+float stack_pop_float(stack s) {
+	float *fl = stack_pop(s);
+	float return_float = *fl;
+	free(fl);
+	return return_float;
+}
+
+double stack_pop_double(stack s) {
+	double *d = stack_pop(s);
+	double return_double = *d;
+	free(d);
+	return return_double;
+}
+
+long double stack_pop_long_double(stack s) {
+	long double *ld = stack_pop(s);
+	long double return_long_double = *ld;
+	free(ld);
+	return return_long_double;
+}
+
+char* stack_pop_string(stack s) {
+	return stack_pop(s);
 }
 
 void* stack_peek(stack s) {
-	if (stack_size(s) == 0) {
-		return NULL;
-	}
 	return s->head->data;
+}
+
+char stack_peek_char(stack s) {
+	return *(char *)s->head->data;
+}
+
+unsigned char stack_peek_uchar(stack s) {
+	return *(unsigned char *)s->head->data;
+}
+
+short stack_peek_short(stack s) {
+	return *(short *)s->head->data;
+}
+
+unsigned short stack_peek_ushort(stack s) {
+	return *(unsigned short *)s->head->data;
+}
+
+int stack_peek_int(stack s) {
+	return *(int *)s->head->data;
+}
+
+unsigned int stack_peek_uint(stack s) {
+	return *(unsigned int *)s->head->data;
+}
+
+long stack_peek_long(stack s) {
+	return *(long *)s->head->data;
+}
+
+unsigned long stack_peek_ulong(stack s) {
+	return *(unsigned long *)s->head->data;
+}
+
+long long stack_peek_long_long(stack s) {
+	return *(long long *)s->head->data;
+}
+
+unsigned long long stack_peek_ulong_long(stack s) {
+	return *(unsigned long long *)s->head->data;
+}
+
+float stack_peek_float(stack s) {
+	return *(float *)s->head->data;
+}
+
+double stack_peek_double(stack s) {
+	return *(double *)s->head->data;
+}
+
+long double stack_peek_long_double(stack s) {
+	return *(long double *)s->head->data;
+}
+
+char* stack_peek_string(stack s) {
+	return s->head->data;
+}
+
+bool stack_is_empty(stack s) {
+	return stack_size(s);
+}
+
+size_t stack_size(stack s) {
+	return s->size;
 }
